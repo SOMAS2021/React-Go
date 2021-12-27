18b99616d53f7a5db6f382df6cffc41c7b8d0e58
Udai <udai.arneja11ua@gmail.com>
2021-12-27 10:23:10 +0000

    Going through the tutorial

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// define our WebSocket endpoint
+func serveWs(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Host)
+
+	// upgrade this connection to a WebSocket
+	// connection
+	ws, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	// listen indefinitely for new messages coming
+	// through on our WebSocket connection
+	reader(ws)
+}
+
+func setupRoutes() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Simple Server")
+	})
+	// mape our `/ws` endpoint to the `serveWs` function
+	http.HandleFunc("/ws", serveWs)
+}
 
 func main() {
-  fmt.Println("Chat App v0.01")
+	fmt.Println("Chat App v0.01")
+	setupRoutes()
+	http.ListenAndServe(":8080", nil)
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { connect, sendMsg } from "./api";
+import Header from './components/Header/Header'
+import ChatHistory from "./components/ChatHistory";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chatHistory: []
+    }
+  }
+
+  componentDidMount(){
+    connect((msg) => {
+      console.log("New Message")
+      this.setState(prevState => ({
+        chatHistory: [...this.state.chatHistory, msg]
+      }))
+      console.log(this.state);
+    })
+  }
+
+  send() {
+    console.log("hello");
+    sendMsg("hello");
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+        <ChatHistory chatHistory={this.state.chatHistory} />
+        <button onClick={this.send}>Hit</button>
+      </div>
+    );
+  }
 }
 
 export default App;
